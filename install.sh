bfd673dd9b8adab0925767d8434007a8246b245d
Joshua Mervine <joshua@mervine.net>
2011-10-05 16:10:15 -0700

    adding vim-config question

--- install.sh
+++ install.sh
@@ -50,7 +50,12 @@ do_file "gitconfig"
 echo ""
 echo "Would you like to install my 'vim-config' as well? [y/N]:"
 read CONTINUE
+
 if [[ $CONTINUE =~ "y" ]] || [[ $CONTINUE =~ "Y" ]]; then
-	bash < <(curl -s https://raw.github.com/jmervine/vim-config/master/install.sh)
+	echo "I will be setting up vim-config as well."
+else 
+	exit 0
 fi
+
+/bin/bash < <(curl -s https://raw.github.com/jmervine/vim-config/master/install.sh)
 
